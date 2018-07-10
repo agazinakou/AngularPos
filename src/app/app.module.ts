6beca7593b0ad5d7ed83cadc7ff450cc017bd0bb
Azinakou <agazinakou@gmail.com>
2018-07-10 15:13:17 +0000

    Login ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { ReactiveFormsModule } from '../../node_modules/@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/shared/header/header.component';
-import { FooterComponent } from './component/shared/footer/footer.component';
-import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './component/auth/login/login.component';
 import { SignupComponent } from './component/auth/signup/signup.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { SignupComponent } from './component/auth/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
