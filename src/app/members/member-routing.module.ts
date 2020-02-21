1a5d64bddb01efc801aab218c3986023e47095b2
azinakou <agazinakou@gmail.com>
2020-02-21 16:39:10 +0000

    manage categories

--- src/app/members/member-routing.module.ts
+++ src/app/members/member-routing.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../components/private/dashboard/dashboard/dashboard.component';
 import { CategoriesComponent } from '../components/private/categories/categories/categories.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 const routes: Routes = [
-  { 
-    path: 'admin/dashboard', 
-    component: DashboardComponent 
+  {
+    path: 'admin/dashboard',
+    component: DashboardComponent
   },
-  { 
-    path: 'admin/categories', 
-    component: CategoriesComponent 
+  {
+    path: 'admin/categories',
+    component: CategoriesComponent
   },
 ];
 
@@ -24,6 +25,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ]
 })
