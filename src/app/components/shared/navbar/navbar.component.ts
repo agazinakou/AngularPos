72aa2e6b7e0b783e901a4254a5845ed77d5259c4
Lamine AZINAKOU <agazinakou@gmail.com>
2021-09-01 16:43:16 +0000

    Migrate to esLint

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
